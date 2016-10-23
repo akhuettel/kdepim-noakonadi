b848fab9d64a92a5b7e9e11bb2ed45b303b5e75e
Andreas K. Hüttel <dilfridge@gentoo.org>
2016-10-23 20:16:47 +0200

    Drop kontact

--- kontact/Messages.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-$EXTRACTRC src/*.kcfg >> rc.cpp || exit 11
-$EXTRACTRC $(find . -name "*.ui" -o -name "*.rc") >> rc.cpp || exit 12
-$XGETTEXT rc.cpp src/*.cpp plugins/*/*.cpp plugins/*/*.h -o $podir/kontact.pot
